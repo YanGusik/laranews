130a45eb53fb04029a8922a5527cb6c117c1f48a
YanGusik <minibaev2001@yandex.ru>
2020-01-06 20:32:56 +0400

    DEV

---
+++ app/Console/Commands/ParsePostsCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Service\ParseLaravelNews;
+use Illuminate\Console\Command;
+
+class ParsePostsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'parse:posts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parse posts from laravel news sites and add data to the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Start parsing");
+
+        try
+        {
+            $news = new ParseLaravelNews();
+            $news->parse() ? $this->info("Finished parsing") : $this->error("Failed parsing");
+        }
+        catch (\Exception $ex)
+        {
+            $this->warn("Site architecture has changed {$ex->getMessage()}");
+        }
+    }
+}
---
+++ app/Interfaces/ParseSiteInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces;
+
+interface ParseSiteInterface
+{
+    public function parse();
+}
